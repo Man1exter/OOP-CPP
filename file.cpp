6040509e467ce200dc81fd2fe7143e43daa1b724
Mariusz Perzynski <m.perzyk@interia.pl>
2020-12-28 12:59:46 +0100

    create public and private class

--- file.cpp
+++ file.cpp
@@ -1,10 +1,30 @@
 #pragma​ warning(disable: 4996)
 #include <iostream>
 #include <string>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
 
 using namespace std;
 
-int main (){
+class liczby{
+
+
+
+
+private:
+czytajDane();
+sortujDane();
+
+public:
+wyswietlWynik();
+}
+
+
+
+
+
+int main(){
 
 
 
