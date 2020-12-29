8988c03ca9e76dcede62e2a189b9500b5f5af1c4
Mariusz Perzynski <m.perzyk@interia.pl>
2020-12-29 11:16:32 +0100

    delete useless libraries

--- file.cpp
+++ file.cpp
@@ -1,26 +1,32 @@
 #pragma​ warning(disable: 4996)
 #include <iostream>
-#include <string>
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
+#include <algorithm>
 
 using namespace std;
 
 
-int iloscLiczb;
-int liczby;
+
+int tab[]
+
+
 
 class liczby{
 
-
 private:
-czytajDane();
-sortujDane();
+
+ int iloscLiczb;
+ int liczby;
+ void czytajDane();
+ void sortujDane();
 
 public:
-wyswietlWynik();
-}
+
+void wyswietlWynik();
+
+};
 
 
 
@@ -35,3 +41,7 @@ int main(){
 
 return 0;
 }
+// Napisz samodzielnie,  zgodnie z zasadami programowania obiektowego program, który posortuje​ n​ liczb.
+//  Klasa powinna zawierać dwie metody prywatne:  ●  czytaj_dane()​ - odczytuje dane i umieszcza je w tablicy ● sortuj_dane() ​- sortuje dane,
+//   korzystając z wybranego algorytmu
+//   oraz jedna metodę publiczną:  ●  wyswietl_wynik()​ - prezentuje zawartość posortowanej tablicy na ekranie monitora
