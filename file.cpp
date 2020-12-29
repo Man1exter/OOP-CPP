57135645301ec71bfd90a6237f512cbffbf65c3a
Mariusz Perzynski <m.perzyk@interia.pl>
2020-12-29 18:21:42 +0100

    basic code

--- file.cpp
+++ file.cpp
@@ -1,60 +1,27 @@
 #include <iostream>
 #include <algorithm>
 
-
 using namespace std;
 
 
-const int LIMIT = 256;
-
-class Dane
+class Panel
 {
-    double liczby[LIMIT];
-    int suma;
-public:
-    Dane() {suma = 0;}
-    int wprowadz_dane();
-    void sortuj(int ilosc);
 
 private:
-    void wyswietl_wynik(int ilosc);
+
+public:
+
 };
+
+
+
 
 int main()
 {
-    Dane Elo;
-    int wartosci = Elo.wprowadz_dane();
-    Elo.sortuj(wartosci);
-    Elo.wyswietl_wynik(wartosci);
-    return 0;
-}
-int Dane::wprowadz_dane()
-{
-    cout << "Podaj liczby (znak niebedacy cyfra konczy wprowadzanie):\n";
-    for (int i = 0; i < LIMIT; i++)
-    {
-        cin >> liczby[i];
-        if (!cin)
-        {
-            cin.clear();
-            cin.sync();
-            break;
-        }
-        ++suma;
-    }
-    return suma;
-}
 
-void Dane::sortuj(int ilosc)
-{
-    sort (liczby, liczby + ilosc);
-}
 
-void Dane::wyswietl_wynik(int ilosc)
-{
-    cout << endl << "Posortowane wartosci:\n";
-    for (int i = 0; i < ilosc; i++)
-        cout << liczby[i] << endl;
+
+return 0;
 }
 
 // Napisz samodzielnie,  zgodnie z zasadami programowania obiektowego program, który posortuje​ n​ liczb.
