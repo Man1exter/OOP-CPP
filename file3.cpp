5622aa9a73ca2f4aa26709024e6c0df5cad5dc85
Mariusz Perzynski <m.perzyk@interia.pl>
2021-01-03 15:14:33 +0100

    add space bettwen line in text(comment)

--- file3.cpp
+++ file3.cpp
@@ -14,8 +14,12 @@ int main(){
     return 0;
 }
 
-// Zaprojektuj klasę, której zadaniem jest obliczenie pierwiastków równania kwadratowegoax​2​+bx+c​ = 0.
+// Zaprojektuj klasę, której zadaniem jest obliczenie pierwiastków równania kwadratowego ax​2​ + bx + c​ = 0.
+
 // Klasa powinna zawierać następujące metody:
+
 // ●podaj_wspolczynniki​ - zapisuje w odpowiednich ​polach klasy​ odczytane wartości współczynników​ a,b,c,​zapytanie o współczynniki powinno zostać zainicjalizowaneprzez konstruktor klasy
+
 // ●oblicz_pierwiastki​ - oblicza pierwiastki wykorzystując instrukcję wyboru ​switch
+
 // ●wyswietl_wynik​
